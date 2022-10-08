0376eb4e03346dfdbda52bb7d88d0082ee1aac9a
peytonmscott <pms0020@auburn.edu>
2022-10-08 12:29:30 -0500

    Initial Commit

---
+++ Shared/Module1ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module1ProjectApp.swift
+//  Shared
+//
+//  Created by Peyton Scott on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Module1ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
